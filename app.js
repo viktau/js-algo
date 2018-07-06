d3edbf30a0d49d1303b800a41abb6288129c6c62
Віктор Татауров <tataurov@KPNU.KM.UA>
2018-07-06 14:59:00 +0300

    Added algo for triangle

--- app.js
+++ app.js
@@ -8,4 +8,9 @@ function writeAlgoDemo1() {
 
 }
 
-function writeAlgoDemo2() {}
+function writeAlgoDemo2() {
+    var side_a = Math.random() * 10 + 5;
+    var side_b = Math.random() * 10 + 5;
+    document.write("<p>Side a=" + side_a + "</p><p>Side b=" + side_b + "</p>");
+    document.write("<p>Площадь прямоугольного треугольника" + side_a + side_b / 2 + "</p>");
+}
