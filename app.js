e7f97e587ff75bb152ee290ab7592afc11f9ec10
Віктор Татауров <tataurov@KPNU.KM.UA>
2018-07-06 14:52:26 +0300

    Changes for next implementation of algorithms

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
-function writeAlgoDemo() {
+function writeAlgoDemo1() {
     var side_a = Math.random() * 10 + 5;
     var side_b = Math.random() * 10 + 5;
-    document.write("Side a=" + side_a + "Side b" + side_b);
-    document.write("Oбьем куба" + side_a * side_a * side_a);
-    document.write("Площадь квадрата" + side_a * side_a);
-    document.write("Площадь прямокутника" + side_a * side_b);
+    document.write("<p>Side a=" + side_a + "</p><p>Side b=" + side_b + "</p>");
+    document.write("<p>Oбьем куба" + side_a * side_a * side_a + "</p>");
+    document.write("<p>Площадь квадрата" + side_a * side_a + "</p>");
+    document.write("<p>Площадь прямокутника" + side_a * side_b + "</p>");
 
 }
+
+function writeAlgoDemo2() {}
