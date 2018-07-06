46b424ca49ff96ea26fd11201181f7fbc155dc5e
Віктор Татауров <tataurov@KPNU.KM.UA>
2018-07-06 14:34:56 +0300

    Added javascript code

---
+++ app.js
@@ -0,0 +1,9 @@
+function writeAlgoDemo() {
+    var side_a = Math.random() * 10 + 5;
+    var side_b = Math.random() * 10 + 5;
+    document.write("Side a=" + side_a + "Side b" + side_b);
+    document.write("Oбьем куба" + side_a * side_a * side_a);
+    document.write("Площадь квадрата" + side_a * side_a);
+    document.write("Площадь прямокутника" + side_a * side_b);
+
+}
